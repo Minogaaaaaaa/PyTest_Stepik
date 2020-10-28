c801574971f5bfa52365ddf900364a6f81606cf2
Minogaaaaaaa <73341881+Minogaaaaaaa@users.noreply.github.com>
2020-10-28 15:44:18 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,6 +6,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_have_button(browser):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/" 
     browser.get(link)
+    #time.sleep(30)
     button = browser.find_elements_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
     assert len(button) == 1, f"Button not found!"
     
